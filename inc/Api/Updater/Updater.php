9f8bf33a1521415335ba2e816db63f7bc3014bea
Zaman <personalzaman@gmail.com>
2021-04-28 12:57:10 +0600

    default tutor reset pass form styling

--- inc/Api/Updater/Updater.php
+++ inc/Api/Updater/Updater.php
@@ -14,91 +14,92 @@ defined( 'ABSPATH' ) || exit;
  */
 class Updater {
 
-    /**
-     * Private $api_endpoint
-     * 
-     * @var string
-     */
-    private $api_endpoint;
-
-    /**
-     * Private $theme_name
-     * 
-     * @var string
-     */
-    private $theme_name;
-
-    /**
-     * Private $theme_version
-     * 
-     * @var string
-     */
-    private $theme_version;
-
-    /**
-     * Register
-     */
-    public function register() {
-        $theme               = wp_get_theme();
-		$this->theme_name    = $theme->get( 'TextDomain' );
-		$this->theme_version = $theme->get( 'Version' );
-        $this->api_endpoint  = 'https://www.themeum.com/wp-json/themeum-license/v2/';
-        
-		add_filter( 'pre_set_site_transient_update_themes', array( $this, 'check_for_update' ) );
-    }
-
-    /**
-     * Check update api
-     * 
-     * @param string
-     * 
-	 * @return array|bool|mixed|object
+	/**
+	 * Meta
+	 * 
+	 * @var $meta
 	 */
-	public function check_for_update_api( $request_from = '' ) {
+	private $meta;
+
+	/**
+	 * Api endpoint
+	 * 
+	 * @var $api_end_point
+	 */
+	private $api_end_point = 'https://www.themeum.com/wp-json/themeum-license/v2/';
+
+	/**
+	 * Construct
+	 * 
+	 * @param array $meta
+	 */
+	public function __construct( array $meta ) {
+		
+		$this->meta         = $meta;
+		$force_check        = isset( $this->meta['force_update_check'] ) && $this->meta['force_update_check'] === true;
+		$update_hook_prefix = $force_check ? '' : 'pre_set_';
+
+		add_filter( $update_hook_prefix . 'site_transient_update_themes', array( $this, 'check_for_update' ) );
+	}
+
+	/**
+	 * @return array|bool|mixed|object
+	 *
+	 * Get update information
+	 */
+	public function check_for_update_api() {
+		
+		$license_info = $this->get_license();
+		$license_key = $license_info ? $license_info['license_key'] : '';
 
 		$params = array(
 			'body' => array(
-				'action'       => 'check_update_free',
-				'product_slug' => $this->theme_name,
-				'request_from' => $request_from,
+				'action'        => 'check_update_by_license',
+				'license_key'   => $license_key,
+				'product_slug'  => $this->product_slug,
 			),
 		);
 
-		// Make the POST request.
-		$request = wp_remote_post( $this->api_endpoint . 'check-update-free', $params );
-
+		// Make the POST request
+		$request = wp_remote_post($this->api_end_point . 'check-update', $params);
 		$request_body = false;
-		// Check if response is valid.
-		if ( ! is_wp_error( $request ) || 200 === wp_remote_retrieve_response_code( $request ) ) {
-            $request_body = json_decode( $request['body'] );
+		
+		// Check if response is valid
+		if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
+			$request_body = json_decode($request['body']);
 		}
+
 		return $request_body;
-    }
+	}
 
 	/**
-     * Check for update
-     * 
 	 * @param $transient
 	 *
 	 * @return mixed
 	 */
-	public function check_for_update( $transient ) {
-		if ( empty( $transient->checked[$this->theme_name] ) ) {
-			return $transient;
-		}
+	public function check_for_update($transient) {
 
-		$request_body = $this->check_for_update_api( 'update_check' );
+		$base_name = $this->meta['product_basename'];
 
-		if ( $request_body && $request_body->success ) {
-			if ( version_compare( $this->theme_version, $request_body->data->version, '<' ) ) {
-				$transient->response[$this->theme_name] = array(
-					'new_version' => $request_body->data->version,
-					'package'     => $request_body->data->download_url,
-					'url'         => $request_body->data->url,
+		$request_body = $this->check_for_update_api();
+
+		if (!empty($request_body->success) && $request_body->success) {
+			if (version_compare($this->meta['current_version'], $request_body->data->version, '<')) {
+				
+				$update_info = array(
+					'new_version'   => $request_body->data->version,
+					'package'       => $request_body->data->download_url,
+					'tested'        => $request_body->data->tested_wp_version,
+					'slug'          => $base_name,
+					'url'           => $request_body->data->url,
 				);
 
+				$transient->response[$base_name] = $this->meta['product_type']=='plugin' ? (object)$update_info : $update_info;
+
+				update_option( $this->error_message_key, $request_body->data->error_message );
 			}
-        }
+		}
+
 		return $transient;
 	}
 }
