779b90b587ffd94c47354ac0833ffa6fdeb5fc86
Md Rashed Hossain <wprashed@icloud.com>
2022-11-10 13:18:04 +0600

    Update form-coupon.php

--- woocommerce/checkout/form-coupon.php
+++ woocommerce/checkout/form-coupon.php
@@ -25,20 +25,21 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 <div class="woocommerce-form-coupon-toggle">
 	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', esc_html__( 'Have a coupon?', 'tutorstarter' ) . ' <a href="#" class="showcoupon">' . esc_html__( 'Click here to enter your code', 'tutorstarter' ) . '</a>' ), 'notice' ); ?>
 </div>
+<div class="tutorstarter-checkout-container tutorstarter-cart-container">
+	<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
 
-<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
+		<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'tutorstarter' ); ?></p>
 
-	<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'tutorstarter' ); ?></p>
+		<p class="form-row form-row-first">
+			<label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', 'tutorstarter' ); ?></label>
+			<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'tutorstarter' ); ?>" id="coupon_code" value="" />
+		</p>
 
-	<p class="form-row form-row-first">
-		<label for="coupon_code" class="screen-reader-text"><?php esc_html_e( 'Coupon:', 'tutorstarter' ); ?></label>
-		<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'tutorstarter' ); ?>" id="coupon_code" value="" />
-	</p>
+		<p class="form-row form-row-last">
+			<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'tutorstarter' ); ?>"><?php esc_html_e( 'Apply coupon', 'tutorstarter' ); ?></button>
+		</p>
 
-	<p class="form-row form-row-last">
-		<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'tutorstarter' ); ?>"><?php esc_html_e( 'Apply coupon', 'tutorstarter' ); ?></button>
-	</p>
-
-	<div class="clear"></div>
-</form>
+		<div class="clear"></div>
+	</form>
+</div>
 
