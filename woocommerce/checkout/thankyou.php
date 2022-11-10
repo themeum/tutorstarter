7b18d197601c9e57d36d10ff1779288a14d57908
Md Rashed Hossain <wprashed@icloud.com>
2022-11-10 13:50:22 +0600

    Update thankyou.php

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -2,8 +2,6 @@
 /**
  * Thankyou page
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/checkout/thankyou.php.
- *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
  * maintain compatibility. We try to do this as little as possible, but it does
