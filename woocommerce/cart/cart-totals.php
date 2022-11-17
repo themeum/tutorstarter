536ef967f7a4ff154ca4300ce45d6076d6f88c1c
Md Rashed Hossain <wprashed@icloud.com>
2022-11-17 13:28:42 +0600

    fix coupon system
    Update: Improved WooCommerce coupon system on the cart & checkout page

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -2,14 +2,6 @@
 /**
  * Cart totals
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart-totals.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
  * @version 2.3.6
@@ -18,26 +10,32 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
+
+<!-- Cart Total -->
 <div class="cart_totals <?php echo ( WC()->customer->has_calculated_shipping() ) ? 'calculated_shipping' : ''; ?>">
 
 	<?php do_action( 'woocommerce_before_cart_totals' ); ?>
 
-	
-
+	<!-- Shop Table -->
 	<table cellspacing="0" class="shop_table shop_table_responsive">
 
+		<!-- Subtotal -->
 		<tr class="cart-subtotal">
 			<th><?php esc_html_e( 'Subtotal', 'tutorstarter' ); ?></th>
 			<th data-title="<?php esc_attr_e( 'Subtotal', 'tutorstarter' ); ?>"><?php wc_cart_totals_subtotal_html(); ?></th>
 		</tr>
+		<!-- Subtotal /-->
 
+		<!-- Coupon -->
 		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
 			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
 				<th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
 				<td data-title="<?php echo esc_attr( wc_cart_totals_coupon_label( $coupon, false ) ); ?>"><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
 			</tr>
 		<?php endforeach; ?>
-
+		<!-- Coupon /-->
+		
+		<!-- Shipping -->
 		<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
 
 			<?php do_action( 'woocommerce_cart_totals_before_shipping' ); ?>
@@ -54,54 +52,60 @@ defined( 'ABSPATH' ) || exit;
 			</tr>
 
 		<?php endif; ?>
-
+		<!-- Shipping /-->
+		
+		<!-- Fee -->
 		<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
 			<tr class="fee">
 				<th><?php echo esc_html( $fee->name ); ?></th>
 				<td data-title="<?php echo esc_attr( $fee->name ); ?>"><?php wc_cart_totals_fee_html( $fee ); ?></td>
 			</tr>
 		<?php endforeach; ?>
-
+		<!-- Fee /-->
+		
+		<!-- Price Including Tax -->
 		<?php
-		if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
-			$taxable_address = WC()->customer->get_taxable_address();
-			$estimated_text  = '';
+			if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
+				$taxable_address = WC()->customer->get_taxable_address();
+				$estimated_text  = '';
 
-			if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
-				/* translators: %s location. */
-				$estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'tutorstarter' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
-			}
+				if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
+					/* translators: %s location. */
+					$estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'tutorstarter' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
+				}
 
-			if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
-				foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+				if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
+					foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+						?>
+						<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+							<th><?php echo esc_html( $tax->label ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
+							<td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+						</tr>
+						<?php
+					}
+				} else {
 					?>
-					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-						<th><?php echo esc_html( $tax->label ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
-						<td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+					<tr class="tax-total">
+						<th><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
+						<td data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
 					</tr>
 					<?php
 				}
-			} else {
-				?>
-				<tr class="tax-total">
-					<th><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
-					<td data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
-				</tr>
-				<?php
 			}
-		}
 		?>
+		<!-- Price Including Tax /-->
 
+		<!-- Order Total -->
 		<?php do_action( 'woocommerce_cart_totals_before_order_total' ); ?>
-
-		<tr class="order-total">
-			<th><?php esc_html_e( 'Total', 'tutorstarter' ); ?></th>
-			<td data-title="<?php esc_attr_e( 'Total', 'tutorstarter' ); ?>"><?php wc_cart_totals_order_total_html(); ?></td>
-		</tr>
-
+			<tr class="order-total">
+				<th><?php esc_html_e( 'Total', 'tutorstarter' ); ?></th>
+				<td data-title="<?php esc_attr_e( 'Total', 'tutorstarter' ); ?>"><?php wc_cart_totals_order_total_html(); ?></td>
+			</tr>
 		<?php do_action( 'woocommerce_cart_totals_after_order_total' ); ?>
+		<!-- Order Total /-->
 
 	</table>
+	<!-- Shop Table /-->
 
 	<div class="wc-proceed-to-checkout">
 		<?php do_action( 'woocommerce_proceed_to_checkout' ); ?>
@@ -110,3 +114,4 @@ defined( 'ABSPATH' ) || exit;
 	<?php do_action( 'woocommerce_after_cart_totals' ); ?>
 
 </div>
+<!-- Cart Total /-->
