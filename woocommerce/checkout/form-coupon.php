99ca9de43ca2ef05e3d3e8c8635d4bb79c2edafb
nur-alam <nuralam862@gmail.com>
2026-05-18 13:39:24 +0600

    chore: update woocommerce templates, add phpcs
    - Update WooCommerce cart and checkout templates to include standard override documentation, correct version headers and fix PHP syntax/indentation
    - Fix accessibility issues on cart item remove links by adding proper role, class and product name in aria labels
    - Clean up SASS files: comment out unused password input positioning and fix indentation
    - Add phpcs.xml.dist configuration for WordPress coding standards with custom exclusions and tutorstarter text domain

--- woocommerce/checkout/form-coupon.php
+++ woocommerce/checkout/form-coupon.php
@@ -2,9 +2,17 @@
 /**
  * Checkout coupon form
  *
- * @see https://docs.woocommerce.com/document/template-structure/
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-coupon.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 10.0.0
+ * @version 9.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -45,4 +53,3 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 		<div class="clear"></div>
 	</form>
 </div>
-
